14a3b939206015202e1b6295f8da49efaa23ac30
jeffary.he <281889448@qq.com>
2020-12-02 14:55:41 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -4,8 +4,8 @@ package org.devops
 def PrintMes(value,color){
     //定义字典
     colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
-              'blue'  : "\033[34m ${value} \033[0m",
-              'green' : "[1;32m>>>>>>>>>>${value}>>>>>>>>>>[m",
+              'blue'  : "\033[34m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
+              'green' : "\033[32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
               'green1' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ]
     //调用
     ansiColor('xterm') {
