dc12c4b78555784d6fb301f1a3c589e25d1f21c5
jeffary.he <281889448@qq.com>
2020-12-02 14:34:43 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,11 +1,13 @@
 package org.devops
 
-//格式化输出
+//格式化输出-->信息和颜色传参
 def PrintMes(value,color){
+    //定义字典
     colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
               'blue'  : "\033[47;34m ${value} \033[0m",
               'green' : "[1;32m>>>>>>>>>>${value}>>>>>>>>>>[m",
               'green1' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ]
+    //调用
     ansiColor('xterm') {
         println(colors[color])
     }
