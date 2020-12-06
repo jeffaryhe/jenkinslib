adf0d7bec8d4c3378c3cc173eac497102bab4cce
jeffary.he <281889448@qq.com>
2020-12-06 18:03:03 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -3,7 +3,7 @@ package org.devops
 //构建类型,定义一个方法Build,buildType从jenkins的传参中获取，buildshell从jenkins的传参中获取
 def Build(buildType,mvnoptions){
       //定义一个list为所需构建工具。
-      //def buildTools = ["mvn":"mvn","ant":"ant","gradle":"gradle","npm":"npm"]
+      def buildTools = ["mvn":"mvn","ant":"ant","gradle":"gradle","npm":"npm"]
       
       println("当前选择的构建类型为 ${buildType}")
       //从git全局配置工具，获取构建工具的所在目录
