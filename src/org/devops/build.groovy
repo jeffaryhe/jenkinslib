d3fd649164ae8c7df932f7b0dcd292b0d9be6af5
jeffary.he <281889448@qq.com>
2020-12-06 18:11:38 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -6,6 +6,7 @@ def Build(buildType,mvnoptions){
       def buildTools = ["mvn":"mvn","ant":"ant","gradle":"gradle","npm":"npm"]
       
       println("当前选择的构建类型为 ${buildType}")
+      println("${mvnoptions}")
       //从git全局配置工具，获取构建工具的所在目录
       buildHome = tool buildTools[buildType]
       sh "${buildHome}/bin/${buildType} -v"
